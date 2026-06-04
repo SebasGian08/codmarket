668f3442f8ea23c465c10f9d03b0a2d4ed0ba9f5
svasquez <svasquez@montgroup.com.pe>
2026-06-03 23:26:37 -0500

    update

--- resources/views/sections/categorias.blade.php
+++ resources/views/sections/categorias.blade.php
@@ -1,4 +1,4 @@
-<section class="deals_section clearfix">
+<section class="deals_section sec_ptb_50 clearfix">
     <div class="container maxw_1460">
 
         <div class="section_title text-center mb-4">
--- resources/views/sections/rubros.blade.php
+++ resources/views/sections/rubros.blade.php
@@ -1,6 +1,6 @@
 @if(isset($rubros) && $rubros->count())
 
-<div class="rubros_section clearfix">
+<div class="rubros_section sec_ptb_50 clearfix">
     <div class="container maxw_1430">
 
         <div class="section_title text-center mb-5">
