63fab6712dc1efee64cc864c35cae6bc00b52c27
svasquez <svasquez@montgroup.com.pe>
2026-08-26 23:42:46 -0500

    update

--- resources/views/sections/rubros.blade.php
+++ resources/views/sections/rubros.blade.php
@@ -14,7 +14,7 @@
             </p>
         </div>
 
-        <div class="row g-4 justify-content-center">
+        <div class="row justify-content-center">
 
             @foreach($rubros as $rubro)
 
@@ -24,7 +24,7 @@
                     : asset('assets/images/default.png');
             @endphp
 
-            <div class="col-lg-4 col-md-4 col-6">
+            <div class="col-lg-4 col-md-4 col-6 mb-3">
 
                 <a href="" class="rubro_card_premium">
 
