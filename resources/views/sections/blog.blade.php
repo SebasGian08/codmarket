72f952ec0d65eb281b7296f7713363ca7fa26fea
svasquez <svasquez@montgroup.com.pe>
2026-09-15 16:32:22 -0500

    update

--- resources/views/sections/blog.blade.php
+++ resources/views/sections/blog.blade.php
@@ -44,7 +44,7 @@
                             </h3>
 
                             <p class="blog_excerpt">
-                                {{ \Illuminate\Support\Str::limit(strip_tags($blog->excerpt ?? ''), 140) }}
+                                {{ \Illuminate\Support\Str::limit(html_entity_decode(strip_tags($blog->excerpt ?? ''), ENT_QUOTES | ENT_HTML5, 'UTF-8'), 140) }}
                             </p>
 
                             <a class="blog_btn" href="{{ route('blog.show', $blog->slug) }}">
